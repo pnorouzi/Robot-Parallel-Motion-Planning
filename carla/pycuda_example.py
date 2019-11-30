d1e0c35dff9027f6d2434da54f4d7243fb005fb5
klaywittler <klaywittler@gmail.com>
2019-11-30 14:25:53 -0500

    set up gmt class

--- carla/pycuda_example.py
+++ carla/pycuda_example.py
@@ -24,7 +24,7 @@ mod = SourceModule("""
         return false;
     }
 
-    __device__ void RSRcost(float *curCost, float *point1,float *point2, int r_min, float *obstacles, int num_obs){
+    __device__ void RSRcost(float *curCost, float *point1,float *point2, float r_min, float *obstacles, int num_obs){
         float PI = 3.1415926535;
 
         float p_c1 [2] = { point1[0] + (r_min * cosf(point1[2] - PI/2)), point1[1] + (r_min * sinf(point1[2] - PI/2))}; 
@@ -125,7 +125,7 @@ mod = SourceModule("""
         return;
     }
 
-    __device__ void LSLcost(float *curCost, float *point1,float *point2, int r_min, float *obstacles, int num_obs){
+    __device__ void LSLcost(float *curCost, float *point1,float *point2, float r_min, float *obstacles, int num_obs){
         float PI = 3.1415926535;
 
         float p_c1 [2] = { point1[0] + (r_min * cosf(point1[2] + PI/2)), point1[1] + (r_min * sinf(point1[2] + PI/2))}; 
@@ -223,7 +223,7 @@ mod = SourceModule("""
         return;
     }
 
-    __device__ void LSRcost(float *curCost, float *point1,float *point2, int r_min, float *obstacles, int num_obs){
+    __device__ void LSRcost(float *curCost, float *point1,float *point2, float r_min, float *obstacles, int num_obs){
         float PI = 3.1415926535;
 
         float p_c1 [2] = { point1[0] + (r_min * cosf(point1[2] + PI/2)), point1[1] + (r_min * sinf(point1[2] + PI/2))}; 
@@ -319,7 +319,7 @@ mod = SourceModule("""
         return;
     }
 
-    __device__ void RSLcost(float *curCost, float *point1,float *point2, int r_min, float *obstacles, int num_obs){
+    __device__ void RSLcost(float *curCost, float *point1,float *point2, float r_min, float *obstacles, int num_obs){
         float PI = 3.1415926535;
 
         float p_c1 [2] = { point1[0] + (r_min * cosf(point1[2] - PI/2)), point1[1] + (r_min * sinf(point1[2] - PI/2))}; 
@@ -415,11 +415,9 @@ mod = SourceModule("""
         return;
     }
 
-    __device__ bool computeDubinsCost(float &cost, float *point1, float *point2, float *obstacles, int num_obs){
+    __device__ bool computeDubinsCost(float &cost, float *point1, float *point2, float r_min, float *obstacles, int num_obs){
 
         float curCost = cost;
-
-        int r_min = 2;
 
         RSRcost(&curCost, point1, point2, r_min, obstacles, num_obs);
         LSLcost(&curCost, point1, point2, r_min, obstacles, num_obs);
@@ -431,6 +429,21 @@ mod = SourceModule("""
         return connected;
     }
 
+    __global__ void dubinConnection(float *cost, int *parent, int *x, int *y, float *states, int *open, int *unexplored, const int xSize, const int *ySize, float *obstacles, int *num_obs, float *radius){
+        const int index = threadIdx.x;
+        if(index > xSize){
+            return;
+        }
+
+        for(int i=0; i < ySize[0]; i++){
+            bool connected = computeDubinsCost(cost[x[index]], &states[x[index]*3], &states[y[i]*3], radius[0], obstacles, num_obs[0]);
+            parent[x[index]] = connected ? y[i]: parent[x[index]];
+            cost[x[index]] = connected ? cost[y[i]] + cost[x[index]] : cost[x[index]];
+            open[x[index]] = connected ? 1 : open[x[index]];
+            open[y[i]] = connected ? 0 : open[y[i]];
+            unexplored[x[index]] = connected ? 0 : unexplored[x[index]];
+        }
+    }
 
     __global__ void wavefront(int *G, int *open, float *cost, float *threshold, const int n){
         const int index = threadIdx.x;
@@ -453,31 +466,6 @@ mod = SourceModule("""
         }      
     }
 
-    __device__ bool dubinCost(float &cost, float *x, float *y){
-        float new_cost = powf(x[0]-y[0],2) + powf(x[1]-y[1],2) + powf(x[2]-y[2],2);
-        bool connected = new_cost < cost;
-        cost = connected ? new_cost : cost;
-        return connected;
-
-    }
-
-    __global__ void dubinConnection(float *cost, int *parent, int *x, int *y, float *states, int *open, int *unexplored, const int xSize, const int *ySize, float *obstacles, int *num_obs){
-        const int index = threadIdx.x;
-        if(index > xSize){
-            return;
-        }
-
-        for(int i=0; i < ySize[0]; i++){
-            bool connected = computeDubinsCost(cost[x[index]], &states[x[index]*3], &states[y[i]*3], obstacles, num_obs[0]);
-            //bool connected = dubinCost(cost[x[index]], &states[x[index]*3], &states[y[i]*3]);
-            parent[x[index]] = connected ? y[i]: parent[x[index]];
-            cost[x[index]] = connected ? cost[y[i]] + cost[x[index]] : cost[x[index]];
-            open[x[index]] = connected ? 1 : open[x[index]];
-            open[y[i]] = connected ? 0 : open[y[i]];
-            unexplored[x[index]] = connected ? 0 : unexplored[x[index]];
-        }
-    }
-
     __global__ void compact(int *x, int *scan, int *indicator, int *waypoints, const int n){
         const int index = threadIdx.x;
         if(index > n){
@@ -490,138 +478,194 @@ mod = SourceModule("""
     }
 """)
 
+wavefront = mod.get_function("wavefront")
+neighborIndicator = mod.get_function("neighborIndicator")
+exclusiveScan = ExclusiveScanKernel(np.int32, "a+b", 0)
+compact = mod.get_function("compact")
+dubinConnection = mod.get_function("dubinConnection")
+
+class GMT(object):
+    def __init__(self, init_parameters, debug=False):
+        self._cpu_init(init_parameters, debug)
+        self._gpu_init(debug)
+
+        self.route = []
+        self.start = 0
+
+    def _cpu_init(self, init_parameters, debug):
+        self.states = init_parameters['states']
+        self.n = self.states.shape[0]
+        self.waypoints = np.arange(self.n).astype(np.int32)
+
+        self.neighbors = init_parameters['neighbors']
+        self.num_neighbors = init_parameters['num_neighbors']
+
+        self.cost = np.full(self.n, np.inf).astype(np.float32)
+        self.Vunexplored = np.full(self.n, 1).astype(np.int32)
+        self.Vopen = np.zeros_like(self.Vunexplored).astype(np.int32)
+        
+        if debug:
+            print('neighbors: ', self.neighbors)
+            # print('parents:', self.parent)
+            # print('cost: ', self.cost)
+            # print('Vunexplored: ', self.Vunexplored)
+            # print('Vopen: ', self.Vopen)
+
+    def _gpu_init(self, debug):
+        self.dev_states = cuda.to_gpu(self.states)
+        self.dev_waypoints = cuda.to_gpu(self.waypoints)
+
+        self.dev_n = cuda.to_gpu(np.array([self.n]))
+
+        self.dev_neighbors = cuda.to_gpu(self.neighbors)
+        self.dev_num_neighbors = cuda.to_gpu(self.num_neighbors)
+        self.neighbors_index = cuda.to_gpu(self.num_neighbors)
+        exclusiveScan(self.neighbors_index)
+
+    def step_init(self, iter_parameters, debug):
+        self.cost[self.start] = np.inf
+        self.Vunexplored[self.start] = 1
+        self.Vopen[self.start] = 0
+
+        self.obstacles = iter_parameters['obstacles']
+        self.num_obs = np.array([self.obstacles.shape[0]]).astype(np.int32)
+        self.parent = np.full(self.n, -1).astype(np.int32)
+
+        self.start = iter_parameters['start']
+        self.goal = iter_parameters['goal']
+        self.radius = iter_parameters['radius']
+        self.threshold = np.array([self.radius]).astype(np.float32)
+
+
+        self.cost[self.start] = 0
+        self.Vunexplored[self.start] = 0
+        self.Vopen[self.start] = 1
+
+        if debug:
+            print('parents:', self.parent)
+            print('cost: ', self.cost)
+            print('Vunexplored: ', self.Vunexplored)
+            print('Vopen: ', self.Vopen)
+
+        self.dev_radius = cuda.to_gpu(np.array([self.radius]).astype(np.float32))
+        self.dev_threshold = cuda.to_gpu(self.threshold)
+
+        self.dev_obstacles = cuda.to_gpu(self.obstacles) 
+        self.dev_num_obs = cuda.to_gpu(self.num_obs)
+
+        self.dev_parent = cuda.to_gpu(self.parent)
+        self.dev_cost = cuda.to_gpu(self.cost)
+
+        self.dev_open = cuda.to_gpu(self.Vopen)
+        self.dev_unexplored = cuda.to_gpu(self.Vunexplored)
+
+    def get_path(self):
+        p = self.goal
+        while p != -1:
+            self.route.append(p)
+            p = self.parent[p]
+
+        # del self.route[-1]
+
+    def run_step(self, iter_parameters, iter_limit=100, debug=False):
+        self.step_init(iter_parameters,debug)
+
+        goal_reached = False
+        iteration = 0
+        while True:
+            iteration += 1
+
+            ########## create Wave front ###############
+            dev_Gindicator = cuda.zeros_like(self.dev_open, dtype=np.int32)
+            wavefront(dev_Gindicator, self.dev_open, self.dev_cost, self.dev_threshold, self.dev_n, block=(self.n,1,1), grid=(1,1))
+            self.dev_threshold += self.dev_threshold
+            goal_reached = dev_Gindicator[self.goal].get() == 1
+
+            ######### scan and compact open set to connect neighbors ###############
+            dev_yscan = cuda.to_gpu(self.dev_open)
+            exclusiveScan(dev_yscan)
+            dev_ySize = dev_yscan[-1] + self.dev_open[-1]
+            ySize = int(dev_ySize.get())
+
+            dev_y = cuda.zeros(ySize, dtype=np.int32)
+            compact(dev_y, dev_yscan, self.dev_open, self.dev_waypoints, dev_ySize, block=(self.n,1,1), grid=(1,1))
+            
+            dev_Gscan = cuda.to_gpu(dev_Gindicator)
+            exclusiveScan(dev_Gscan)
+            dev_gSize = dev_Gscan[-1] + dev_Gindicator[-1]
+            gSize = int(dev_gSize.get())
+
+            if ySize == 0:
+                print('### empty open set ###')
+                del self.route[-1]
+                return self.route
+            elif iteration >= iter_limit:
+                print('### iteration limit ###')
+                del self.route[-1]
+                return self.route
+            elif goal_reached:
+                print('### goal reached ###')
+                self.parent = self.dev_parent.get()
+                self.get_path()
+                return self.route
+            elif gSize == 0:
+                print('### threshold skip')
+                continue
+
+            dev_G = cuda.zeros(gSize, dtype=np.int32)
+            compact(dev_G, dev_Gscan, dev_Gindicator, self.dev_waypoints, dev_gSize, block=(self.n,1,1), grid=(1,1))
+            
+
+            ########## creating neighbors of wave front to connect open ###############
+            dev_xindicator = cuda.zeros_like(self.dev_open, dtype=np.int32)
+            neighborIndicator(dev_xindicator, dev_G, self.dev_unexplored, self.dev_neighbors, self.dev_num_neighbors, self.neighbors_index, dev_gSize, block=(gSize,1,1), grid=(1,1))
+
+            dev_xscan = cuda.to_gpu(dev_xindicator)
+            exclusiveScan(dev_xscan)
+            dev_xSize = dev_xscan[-1] + dev_xindicator[-1]
+            xSize = int(dev_xSize.get())
+
+            if xSize == 0:
+                print('### x skip')
+                continue
+
+            dev_x = cuda.zeros(xSize, dtype=np.int32)
+            compact(dev_x, dev_xscan, dev_xindicator, self.dev_waypoints, dev_xSize, block=(self.n,1,1), grid=(1,1))
+
+            ######### connect neighbors ####################
+            # # launch planning
+            dubinConnection(self.dev_cost, self.dev_parent, dev_x, dev_y, self.dev_states, self.dev_open, self.dev_unexplored, dev_xSize, dev_ySize, self.dev_obstacles, self.dev_num_obs, self.dev_radius, block=(xSize,1,1), grid=(1,1))
+
+            if debug:
+                print('######### iteration: ', iteration)
+                print('parents:', self.dev_parent)
+                print('cost: ', self.dev_cost)
+                print('Vunexplored: ', self.dev_unexplored)
+                print('Vopen: ', self.dev_open)
+                print('threshold: ', self.dev_threshold)
+
+                print('goal reached: ', goal_reached)
+                print('y size: ', ySize, 'y: ' , dev_y)
+                print('G size: ', gSize, 'G: ', dev_Gindicator)
+
+                print('x size: ', xSize, 'x: ', dev_x)
+
+
 if __name__ == '__main__':
-
-    wavefront = mod.get_function("wavefront")
-    neighborIndicator = mod.get_function("neighborIndicator")
-    exclusiveScan = ExclusiveScanKernel(np.int32, "a+b", 0)
-    compact = mod.get_function("compact")
-    dubinConnection = mod.get_function("dubinConnection")
-
-    ### CPU INIT ###
-    print('################ CPU INIT ###############')
     states = np.array([[1,2,135], [0,3,30], [0,3,45], [1,2,-90], [1,2,-60], [0,3,-45]]).astype(np.float32)
-    waypoints = np.array([0,1,2,3,4,5]).astype(np.int32)
+
+    neighbors = np.array([1,2, 0,3, 0,3,4, 1,2,4, 2,3,5, 4]).astype(np.int32)
+    num_neighbors = np.array([2, 2, 3, 3, 3, 1]).astype(np.int32)
 
     obstacles = np.array([[10,20,15,15], [-6,-3,-3,-6], [7,10,10,7]]).astype(np.float32)
-    num_obs = np.array([3]).astype(np.int32)
 
     start = 0
     goal = 5
-    n = 6
-    radius = 6
-    threshold = np.array([radius]).astype(np.float32)
+    radius = 2
 
-    neighbors = np.array([1,2, 0,3, 0,3,4, 1,2,4, 2,3,5, 4]).astype(np.int32)
-    print('neighbors: ',neighbors)
-    num_neighbors = np.array([2, 2, 3, 3, 3, 1]).astype(np.int32)
+    init_parameters = {'states':states, 'neighbors':neighbors, 'num_neighbors':num_neighbors}
+    iter_parameters = {'start':start, 'goal':goal, 'radius':radius, 'obstacles':obstacles}
 
-    parent = np.full(n, -1).astype(np.int32)
-    print('parents:', parent)
-
-    cost = np.full(n, np.inf).astype(np.float32)
-    cost[start] = 0
-    print('cost: ', cost)
-
-    Vunexplored = np.full(n, 1).astype(np.int32)
-    Vunexplored[start] = 0
-    print('Vunexplored: ', Vunexplored)
-
-    Vopen = np.zeros_like(Vunexplored).astype(np.int32)
-    Vopen[start] = 1
-    print('Vopen: ', Vopen)
-
-    ### GPU INIT ###
-    print('################ GPU INIT ###############')
-    dev_states = cuda.to_gpu(states)
-    dev_waypoints = cuda.to_gpu(waypoints)
-
-    dev_obstacles = cuda.to_gpu(obstacles) 
-    dev_num_obs = cuda.to_gpu(num_obs)
-
-    dev_threshold = cuda.to_gpu(threshold)
-    dev_n = cuda.to_gpu(np.array([n]))
-
-    dev_neighbors = cuda.to_gpu(neighbors)
-    dev_num_neighbors = cuda.to_gpu(num_neighbors)
-    neighbors_index = cuda.to_gpu(num_neighbors)
-    exclusiveScan(neighbors_index)
-    print('neighbors index: ', neighbors_index)
-
-    dev_parent = cuda.to_gpu(parent)
-    
-    dev_cost = cuda.to_gpu(cost)
-
-    dev_open = cuda.to_gpu(Vopen)
-    dev_unexplored = cuda.to_gpu(Vunexplored)
-
-    print('################ GMT* ###############')
-    ########## algorithm starts ####################
-    i = 0
-    while True:
-        print('######### iteration: ', i)
-        print('parents:', dev_parent)
-        print('cost: ', dev_cost)
-        print('Vunexplored: ', dev_unexplored)
-        print('Vopen: ', dev_open)
-        print('threshold: ', dev_threshold)
-        i += 1
-
-        ########## create Wave front ###############
-        dev_Gindicator = cuda.zeros_like(dev_open, dtype=np.int32)
-        wavefront(dev_Gindicator, dev_open, dev_cost, dev_threshold, dev_n, block=(n,1,1), grid=(1,1))
-        dev_threshold += dev_threshold
-        goal_reached = dev_Gindicator[goal].get() == 1
-
-        ######### scan and compact open set to connect neighbors ###############
-        dev_yscan = cuda.to_gpu(dev_open)
-        exclusiveScan(dev_yscan)
-        dev_ySize = dev_yscan[-1] + dev_open[-1]
-        ySize = int(dev_ySize.get())
-
-        dev_y = cuda.zeros(ySize, dtype=np.int32)
-        compact(dev_y, dev_yscan, dev_open, dev_waypoints, dev_ySize, block=(n,1,1), grid=(1,1))
-        
-        dev_Gscan = cuda.to_gpu(dev_Gindicator)
-        exclusiveScan(dev_Gscan)
-        dev_gSize = dev_Gscan[-1] + dev_Gindicator[-1]
-        gSize = int(dev_gSize.get())
-
-        print('goal reached: ', goal_reached)
-        print('y size: ', ySize, 'y: ' , dev_y)
-        print('G size: ', gSize, 'G: ', dev_Gindicator)
-
-        if ySize == 0 or i == 100:
-            print('### empty open set ###')
-            break
-        elif goal_reached:
-            print('### goal reached ###')
-            break
-        elif gSize == 0:
-            print('### threshold skip')
-            continue
-
-        dev_G = cuda.zeros(gSize, dtype=np.int32)
-        compact(dev_G, dev_Gscan, dev_Gindicator, dev_waypoints, dev_gSize, block=(n,1,1), grid=(1,1))
-        
-
-        ########## creating neighbors of wave front to connect open ###############
-        dev_xindicator = cuda.zeros_like(dev_open, dtype=np.int32)
-        neighborIndicator(dev_xindicator, dev_G, dev_unexplored, dev_neighbors, dev_num_neighbors, neighbors_index, dev_gSize, block=(gSize,1,1), grid=(1,1))
-
-        dev_xscan = cuda.to_gpu(dev_xindicator)
-        exclusiveScan(dev_xscan)
-        dev_xSize = dev_xscan[-1] + dev_xindicator[-1]
-        xSize = int(dev_xSize.get())
-
-        if xSize == 0:
-            print('### x skip')
-            continue
-
-        dev_x = cuda.zeros(xSize, dtype=np.int32)
-        compact(dev_x, dev_xscan, dev_xindicator, dev_waypoints, dev_xSize, block=(n,1,1), grid=(1,1))
-        print('x size: ', xSize, 'x: ', dev_x)
-
-        ######### connect neighbors ####################
-        # # launch planning
-        dubinConnection(dev_cost, dev_parent, dev_x, dev_y, dev_states, dev_open, dev_unexplored, dev_xSize, dev_ySize, dev_obstacles, dev_num_obs, block=(xSize,1,1), grid=(1,1))
+    gmt = GMT(init_parameters, debug=True)
+    route = gmt.run_step(iter_parameters, debug=True)
+    print(route)
